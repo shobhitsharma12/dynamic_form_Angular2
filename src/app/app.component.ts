d8881b0f0e0cd0c22bd133794cf0e75e605f2f9a
Shobhit Sharma <shobhit105220@gmail.com>
2018-12-04 12:20:49 +0000

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { Component, ViewChild } from '@angular/core';
+
+import { FormFieldComponent } from './forms/formfield/form.field.component'
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  @ViewChild('formField') formField: FormFieldComponent;
+
+  name = 'Angular';
+  private field: any = { label: 'Dynamic Label', type: 'string' };
+  private input: any = { type: 'radio' }
+  public formFieldConfig: any = { field: this.field, input: this.input }
+
+  ngAfterViewInit() {
+    const self = this;
+    // window.a = this.formField
+    setTimeout(() => {
+      console.log(self.formField.getValue());
+    }, 5000)
+  }
+
+  onFieldUpdate(data) {
+    console.log(data)
+  }
+}
